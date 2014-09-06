d14610a951bf24447af1bc844e91aed71462d6b7
Jeremy Vandehey <jgvandehey@gmail.com>
2014-09-06 12:46:24 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mobilecleanse::Application.config.secret_token = '14a4f709f105e462d1f1131c92244020ba3b47fe244335c274a0bf40ae034b8c26642cf0a984633615cb93e25246e5cb4e74825fdc5f22f100e2e81e9f835209'
